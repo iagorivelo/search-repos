d62697846989166811bfc15b15c7e649f505eef2
Iago Rivelo <iago.rivelo@gmail.com>
2025-09-24 09:56:18 -0300

    feat: adicionando botão de pesquisa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,15 @@ export default function Home() {
   const [value, setValue] = useState("");
   const router = useRouter();
 
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === "Enter" && value.trim() !== "") {
+  const handleSearch = () => {
+    if (value.trim() !== "") {
       router.push(`/repositorio/${value}`);
+    }
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      handleSearch();
     }
   };
 
@@ -18,14 +24,24 @@ export default function Home() {
       <h1 className="text-white text-center font-bold text-2xl rounded p-2">
         Pesquisar Repositórios
       </h1>
-      <input
-        className="outline-none font-bold text-white bg-indigo-600 rounded p-2 w-full"
-        type="text"
-        placeholder="example: iagorivelo"
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-        onKeyDown={handleKeyDown}
-      />
+
+      <div className="flex gap-2">
+        <input
+          className="outline-none font-bold text-white bg-indigo-600 rounded p-2 w-full"
+          type="text"
+          placeholder="example: iagorivelo"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          onKeyDown={handleKeyDown}
+        />
+
+        <button
+          onClick={handleSearch}
+          className="bg-zinc-700 hover:bg-zinc-800 text-white font-bold rounded p-2 transition"
+        >
+          Buscar
+        </button>
+      </div>
     </main>
   );
 }
